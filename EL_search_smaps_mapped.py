2b2a307285485255c4fe19556faf2e01d0f312b7
Lemuel Torrefiel <lemuel.torrefiel@belleproperty.com>
2025-01-28 12:14:06 +0800

    revised based on the mongodb limit

--- EL_search_smaps_mapped.py
+++ EL_search_smaps_mapped.py
@@ -70,7 +70,7 @@ def perform_etl(officeId, office, smapsID):
                 print(e)
                 print(data)
 
-smaps_new = smaps[612+113:]
+smaps_new = smaps[612+113+590:]
 for z in tqdm(smaps_new, total=len(smaps_new), desc="extracting per smap", ncols=100):
     data = {
         "listingType": "SOLD",
