13a841f29d11aabf49a9631b49199e2dfb5c08bf
lemt-cs <lemt@cloudstaff.com>
2025-02-10 15:38:09 +0800

    EDIT for actions

--- EL_search_smaps_mapped.py
+++ EL_search_smaps_mapped.py
@@ -70,8 +70,8 @@ def perform_etl(officeId, office, smapsID):
                 print(e)
                 print(data)
 
-smaps_new = smaps[99+619+539+656+720+72+333:]
-# smaps_new = smaps.copy()
+# smaps_new = smaps[99+619+539+656+720+72+333:]
+smaps_new = smaps.copy()
 for z in tqdm(smaps_new, total=len(smaps_new), desc="extracting per smap", ncols=100):
     data = {
         "listingType": "SOLD",
